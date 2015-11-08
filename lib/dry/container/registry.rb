626499ceb65a4c6120c8f687b36a79443746ac3b
Andy Holland <andyholland1991@aol.com>
2015-11-08 16:18:33 +0000

    Dup/Freeze string keys

--- lib/dry/container/registry.rb
+++ lib/dry/container/registry.rb
@@ -32,6 +32,7 @@ module Dry
           if container.key?(key)
             fail Error, "There is already an item registered with the key #{key.inspect}"
           else
+            key = key.is_a?(::String) ? key.dup.freeze : key
             container[key] = ::Dry::Container::Item.new(item, options)
           end
         end
