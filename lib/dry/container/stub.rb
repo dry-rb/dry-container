7ba011362074c56b7c3fbcc570fdc486aa650027
Seba Gamboa <me@sagmor.com>
2016-02-16 13:52:55 -0300

    Remove unneeded module

--- lib/dry/container/stub.rb
+++ lib/dry/container/stub.rb
@@ -1,49 +1,47 @@
 module Dry
   class Container
     module Stub
-      module Mixin
-        # Overrides resolve to look into stubbed keys first
-        #
-        # @api public
-        def resolve(key)
-          _stubs.fetch(key) { super }
+      # Overrides resolve to look into stubbed keys first
+      #
+      # @api public
+      def resolve(key)
+        _stubs.fetch(key) { super }
+      end
+
+      # Add a stub to the container
+      def stub(key, value, &block)
+        _stubs[key] = value
+
+        if block
+          yield
+          unstub(key)
         end
 
-        # Add a stub to the container
-        def stub(key, value, &block)
-          _stubs[key] = value
+        self
+      end
 
-          if block
-            yield
-            unstub(key)
-          end
+      # Remove stubbed keys from the container
+      def unstub(*keys)
+        keys = _stubs.keys if keys.empty?
+        keys.each { |key| _stubs.delete(key) }
+      end
 
-          self
-        end
+      # Stubs have already been enabled turning this into a noop
+      def enable_stubs!
+      end
 
-        # Remove stubbed keys from the container
-        def unstub(*keys)
-          keys = _stubs.keys if keys.empty?
-          keys.each { |key| _stubs.delete(key) }
-        end
+      private
 
-        # Stubs have already been enabled turning this into a noop
-        def enable_stubs!
-        end
-
-        private
-
-        # Stubs container
-        def _stubs
-          @_stubs ||= {}
-        end
+      # Stubs container
+      def _stubs
+        @_stubs ||= {}
       end
     end
 
     module Mixin
       # Enable stubbing functionality into the current container
       def enable_stubs!
-        extend Dry::Container::Stub::Mixin
+        extend Dry::Container::Stub
       end
     end
   end
