606d31732acd9f4214e01d59a30b67e58fd64895
Andy Holland <andyholland1991@aol.com>
2016-04-29 02:49:05 +0100

    Setup CC coverage/rubocop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,8 @@ require 'dry/container/stub'
 Dir[Pathname(__FILE__).dirname.join('support/**/*.rb').to_s].each do |file|
   require file
 end
+
+if RUBY_ENGINE == 'rbx'
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
