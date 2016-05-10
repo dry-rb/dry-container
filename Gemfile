73c5dfb151affa57805be0c4d2288167077b0c87
Nikita Shilnikov <ns@latera.ru>
2016-05-10 21:51:55 +0300

    Try to fix coverage

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 gemspec
 
 group :test do
-  gem 'codeclimate-test-reporter', platform: :rbx
+  gem 'codeclimate-test-reporter', platform: :rbx, require: false
 end
 
 group :tools do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,13 @@
 # encoding: utf-8
 
+if RUBY_ENGINE == 'rbx'
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
+
 require 'dry/container'
 require 'dry/container/stub'
 
 Dir[Pathname(__FILE__).dirname.join('support/**/*.rb').to_s].each do |file|
   require file
-end
-
-if RUBY_ENGINE == 'rbx'
-  require 'codeclimate-test-reporter'
-  CodeClimate::TestReporter.start
 end
