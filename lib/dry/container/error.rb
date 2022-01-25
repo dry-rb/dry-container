8a732e059414e7c333cc8947bc5dbf8a874ac3c2
Sean Collins <sean@cllns.com>
2022-01-25 11:21:46 -0400

    Create Dry::Container::KeyError

--- lib/dry/container/error.rb
+++ lib/dry/container/error.rb
@@ -4,5 +4,10 @@ module Dry
   class Container
     # @api public
     Error = Class.new(StandardError)
+
+    KeyError = Class.new(::KeyError)
+    DidYouMean.correct_error(KeyError, DidYouMean::KeyErrorChecker)
+
+    deprecate_constant(:Error)
   end
 end
